1109b24859ed438dd09db66d8ba0a865e30ab8f1
mrroll <58090032+mrroll@users.noreply.github.com>
2022-05-13 03:44:59 +0800

    2.9.0

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
 const plugin = require("tailwindcss/plugin");
 const toChannels = require("./toChannels");
 
-const root = {
-  "--tw-custom-color-brand": toChannels("#6366F1"),
-  "--tw-custom-color-accent": toChannels("#3B82F6"),
-  "--tw-custom-color-background": toChannels("#EEF2FF"),
-  "--tw-custom-color-text": toChannels("#1F2937"),
-  "--tw-custom-color-positive": toChannels("#10B981"),
-  "--tw-custom-color-negative": toChannels("#F97316"),
-  "&.dark-mode": {
-    "--tw-custom-color-text": toChannels("#EEF2FF"),
-    "--tw-custom-color-background": toChannels("#1F2937"),
-  },
-};
-
 module.exports = plugin(({ addBase }) => {
   addBase({
     // Custom Colors which can be used with various Tailwind Classes.
-    ":root": root,
+    ":root": {
+      "--tw-custom-color-brand": toChannels("#6366F1"),
+      "--tw-custom-color-accent": toChannels("#3B82F6"),
+      "--tw-custom-color-background": toChannels("#EEF2FF"),
+      "--tw-custom-color-text": toChannels("#1F2937"),
+      "--tw-custom-color-positive": toChannels("#10B981"),
+      "--tw-custom-color-negative": toChannels("#F97316"),
+
+      "--tw-custom-animation-duration": "150ms",
+
+      "&.dark-mode": {
+        "--tw-custom-color-text": toChannels("#EEF2FF"),
+        "--tw-custom-color-background": toChannels("#1F2937"),
+      },
+    },
     // https://www.joshwcomeau.com/css/custom-css-reset/#digit-root-stacking-context
     // https://css-tricks.com/notes-on-josh-comeaus-custom-css-reset/
     "html, body, #__next, #root": {
