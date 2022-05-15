41dcc7dd486ca8dd78a559abf1f2eff830e547c6
mrroll <58090032+mrroll@users.noreply.github.com>
2022-05-15 20:42:19 +0800

    2.10.0

--- preset.js
+++ preset.js
@@ -27,6 +27,7 @@ module.exports = {
           from: { opacity: 1 },
           to: { opacity: 0 },
         },
+
         "rotate-in": {
           from: { opacity: 0, transform: "rotateX(-10deg) scale(0.95)" },
           to: { opacity: 1, transform: "rotateX(0) scale(1)" },
@@ -35,34 +36,87 @@ module.exports = {
           from: { opacity: 1, transform: "rotateX(0) scale(1)" },
           to: { opacity: 0, transform: "rotateX(-10deg) scale(0.95)" },
         },
+
+        "slide-in-from-top": {
+          from: {
+            opacity: 0,
+            transform:
+              "translateY(calc(var(--tw-custom-animation-slide-length, 100%) * -1))",
+          },
+          to: { opacity: 1, transform: "translateY(0)" },
+        },
         "slide-in-from-right": {
-          from: { opacity: 0, transform: "translateX(100%)" },
+          from: {
+            opacity: 0,
+            transform:
+              "translateX(var(--tw-custom-animation-slide-length, 100%))",
+          },
           to: { opacity: 1, transform: "translateX(0)" },
         },
+        "slide-in-from-bottom": {
+          from: {
+            opacity: 0,
+            transform:
+              "translateY(var(--tw-custom-animation-slide-length, 100%))",
+          },
+          to: { opacity: 1, transform: "translateY(0)" },
+        },
         "slide-in-from-left": {
-          from: { opacity: 0, transform: "translateX(-100%)" },
+          from: {
+            opacity: 0,
+            transform:
+              "translateX(calc(var(--tw-custom-animation-slide-length, 100%) * -1))",
+          },
           to: { opacity: 1, transform: "translateX(0%)" },
+        },
+        "slide-out-to-top": {
+          from: { opacity: 1, transform: "translateY(0)" },
+          to: {
+            opacity: 0,
+            transform:
+              "translateY(calc(var(--tw-custom-animation-slide-length, 100%) * -1))",
+          },
         },
         "slide-out-to-right": {
           from: { opacity: 1, transform: "translateX(0)" },
-          to: { opacity: 0, transform: "translateX(100%)" },
+          to: {
+            opacity: 0,
+            transform:
+              "translateX(var(--tw-custom-animation-slide-length, 100%))",
+          },
+        },
+        "slide-out-to-bottom": {
+          from: { opacity: 1, transform: "translateY(0)" },
+          to: {
+            opacity: 0,
+            transform:
+              "translateY(var(--tw-custom-animation-slide-length, 100%))",
+          },
         },
         "slide-out-to-left": {
           from: { opacity: 1, transform: "translateX(0%)" },
-          to: { opacity: 0, transform: "translateX(-100%)" },
+          to: {
+            opacity: 0,
+            transform:
+              "translateX(calc(var(--tw-custom-animation-slide-length, -100%) * -1))",
+          },
         },
       },
 
       // prettier-ignore
       animation: {
-        "fade-in"            : "fade-in var(--tw-custom-animation-duration) linear",
-        "fade-out"           : "fade-out var(--tw-custom-animation-duration) linear",
-        "rotate-in"          : "rotate-in var(--tw-custom-animation-duration) ease",
-        "rotate-out"         : "rotate-out var(--tw-custom-animation-duration) ease",
-        "slide-in-from-right": "slide-in-from-right var(--tw-custom-animation-duration) ease-in-out",
-        "slide-in-from-left" : "slide-in-from-left var(--tw-custom-animation-duration) ease-in-out",
-        "slide-out-to-right" : "slide-out-to-right var(--tw-custom-animation-duration) ease-in-out",
-        "slide-out-to-left"  : "slide-out-to-left var(--tw-custom-animation-duration) ease-in-out",
+        "fade-in"              : "fade-in var(--tw-custom-animation-duration) linear" ,
+        "fade-out"             : "fade-out var(--tw-custom-animation-duration) linear" ,
+        "rotate-in"            : "rotate-in var(--tw-custom-animation-duration) ease" ,
+        "rotate-out"           : "rotate-out var(--tw-custom-animation-duration) ease" ,
+        "slide-in-from-top"    : "slide-in-from-top var(--tw-custom-animation-duration) ease-in-out" ,
+        "slide-in-from-right"  : "slide-in-from-right var(--tw-custom-animation-duration) ease-in-out" ,
+        "slide-in-from-bottom" : "slide-in-from-bottom var(--tw-custom-animation-duration) ease-in-out" ,
+        "slide-in-from-left"   : "slide-in-from-left var(--tw-custom-animation-duration) ease-in-out" ,
+        "slide-out-to-top"     : "slide-out-to-top var(--tw-custom-animation-duration) ease-in-out" ,
+        "slide-out-to-right"   : "slide-out-to-right var(--tw-custom-animation-duration) ease-in-out" ,
+        "slide-out-to-bottom"  : "slide-out-to-bottom var(--tw-custom-animation-duration) ease-in-out" ,
+        "slide-out-to-left"    : "slide-out-to-left var(--tw-custom-animation-duration) ease-in-out" ,
       },
 
       linearBorderGradients: ({ theme }) => {
