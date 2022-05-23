e47dd95b0c72e6b3ad3a59d05028b60c2e753991
mrroll <58090032+mrroll@users.noreply.github.com>
2022-05-24 03:11:35 +0800

    3.9.0

--- index.js
+++ index.js
@@ -1,69 +1,13 @@
 const plugin = require("tailwindcss/plugin");
 const { base } = require("./theme");
 
-module.exports = plugin(({ addBase, theme }) => {
+module.exports = plugin(({ addBase, addUtilities, theme }) => {
   addBase({
     ":root": {
       ...base,
 
       "--tw-custom-animation-duration": "150ms",
       "--tw-custom-animation-timing-function": "ease",
-
-      h1: {
-        "line-height": theme("lineHeight.none"),
-        "font-size": [
-          theme("fontSize.6xl"),
-          `clamp(${theme("fontSize.6xl")}, 6vw, theme('fontSize.7xl'))`,
-        ],
-      },
-
-      h2: {
-        "line-height": theme("lineHeight.none"),
-        "font-size": [
-          theme("fontSize.5xl"),
-          `clamp(${theme("fontSize.5xl")}, 5vw, theme('fontSize.6xl'))`,
-        ],
-      },
-
-      h3: {
-        "line-height": theme("lineHeight.none"),
-        "font-size": [
-          theme("fontSize.4xl"),
-          `clamp(${theme("fontSize.4xl")}, 4vw, theme('fontSize.5xl'))`,
-        ],
-      },
-
-      h4: {
-        "line-height": theme("lineHeight.none"),
-        "font-size": [
-          theme("fontSize.3xl"),
-          `clamp(${theme("fontSize.3xl")}, 3vw, theme('fontSize.4xl'))`,
-        ],
-      },
-
-      h5: {
-        "line-height": theme("lineHeight.none"),
-        "font-size": [
-          theme("fontSize.2xl"),
-          `clamp(${theme("fontSize.2xl")}, 2vw, theme('fontSize.3xl'))`,
-        ],
-      },
-
-      h6: {
-        "line-height": theme("lineHeight.10"),
-        "font-size": [
-          theme("fontSize.xl"),
-          `clamp(${theme("fontSize.xl")}, 1.5vw, theme('fontSize.2xl'))`,
-        ],
-      },
-
-      "p, li, dl, dt, blockquote, pre": {
-        "line-height": theme("lineHeight.6"),
-        "font-size": [
-          theme("fontSize.base"),
-          `clamp(${theme("fontSize.base")}, 1vw, theme('fontSize.lg'))`,
-        ],
-      },
     },
 
     // https://www.joshwcomeau.com/css/custom-css-reset/
@@ -79,6 +23,64 @@ module.exports = plugin(({ addBase, theme }) => {
     },
     pre: {
       "white-space": "normal",
+    },
+  });
+
+  addUtilities({
+    ".text-h1": {
+      "line-height": theme("lineHeight.none"),
+      "font-size": [
+        theme("fontSize.6xl"),
+        `clamp(${theme("fontSize.6xl")}, 6vw, theme('fontSize.7xl'))`,
+      ],
+    },
+
+    ".text-h2": {
+      "line-height": theme("lineHeight.none"),
+      "font-size": [
+        theme("fontSize.5xl"),
+        `clamp(${theme("fontSize.5xl")}, 5vw, theme('fontSize.6xl'))`,
+      ],
+    },
+
+    ".text-h3": {
+      "line-height": theme("lineHeight.none"),
+      "font-size": [
+        theme("fontSize.4xl"),
+        `clamp(${theme("fontSize.4xl")}, 4vw, theme('fontSize.5xl'))`,
+      ],
+    },
+
+    ".text-h4": {
+      "line-height": theme("lineHeight.none"),
+      "font-size": [
+        theme("fontSize.3xl"),
+        `clamp(${theme("fontSize.3xl")}, 3vw, theme('fontSize.4xl'))`,
+      ],
+    },
+
+    ".text-h5": {
+      "line-height": theme("lineHeight.none"),
+      "font-size": [
+        theme("fontSize.2xl"),
+        `clamp(${theme("fontSize.2xl")}, 2vw, theme('fontSize.3xl'))`,
+      ],
+    },
+
+    ".text-h6": {
+      "line-height": theme("lineHeight.10"),
+      "font-size": [
+        theme("fontSize.xl"),
+        `clamp(${theme("fontSize.xl")}, 1.5vw, theme('fontSize.2xl'))`,
+      ],
+    },
+
+    ".text-p": {
+      "line-height": theme("lineHeight.6"),
+      "font-size": [
+        theme("fontSize.base"),
+        `clamp(${theme("fontSize.base")}, 1vw, theme('fontSize.lg'))`,
+      ],
     },
   });
 });
